60b9beac852860ff30404eaa75ffdbd44cc9c2f2
Vladimir Milyukov <vova.milyukov.94@bk.ru>
2024-07-10 22:01:09 +0300

    added весь текст junit5

--- src/test/java/SearchSelenideJunitTest.java
+++ src/test/java/SearchSelenideJunitTest.java
@@ -6,7 +6,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class SearchSelenideJunit {
+public class SearchSelenideJunitTest {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
